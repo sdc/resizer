a97ff2d504fd2933d33bfc1f880e1b66a2edf5bb
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-12-12 14:49:16 +0000

    Capfile and deploy.rb file.

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+if ENV['environment'] == "production"
+    set :application, "resizer"
+    role :app,  "172.21.1.49"
+    role :web,  "172.21.1.49"
+    role :db,   "172.21.1.49", :primary => true
+    set :keep_releases, 3
+else
+    set :application, "resizer_dev"
+    role :app,  "webdev.southdevon.ac.uk"
+    role :web,  "webdev.southdevon.ac.uk"
+    role :db,   "webdev.southdevon.ac.uk", :primary => true
+    set :keep_releases, 3
+end
+
+default_run_options[:pty] = true
+
+set :repository,"git@github.com:sdc/resizer.git"
+set :branch,    "master"
+set :deploy_to, "/srv/#{application}"
+set :scm, :git
+
+namespace :deploy do
+    %W(start stop restart migrate finalize_update).each do |event|
+        task event do
+            # don't
+        end
+    end
+end
+
+after "deploy:create_symlink" do
+    #don't
+end
